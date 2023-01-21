2b8e89c03a8074f9feccd68f9e587d86dbbb976d
Victor Gila Barreto <gilabarreto@gmail.com>
2023-01-21 14:54:33 -0700

    Create function isDead to handle collision

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const bill = document.getElementById("bill");
+const chair = document.getElementById("chair");
 
+
+// Function to make Bill jump
 function jump() {
+  // Check if is jumping, add jump class and remove it
   if (bill.classList != "jump") {
     bill.classList.add("jump");
 
@@ -10,6 +14,23 @@ function jump() {
   }
 }
 
+// Function to check if Bill hit the chair
+let isDead = setInterval(() => {
+
+  let billRect = bill.getBoundingClientRect();
+  let chairRect = chair.getBoundingClientRect();
+
+  if (chairRect.left < billRect.right &&
+    chairRect.top < billRect.bottom &&
+    chairRect.right > billRect.left &&
+    chairRect.bottom > billRect.top) {
+    alert("Game Over")
+  }
+
+}, 10)
+
+
+// Event listener for keydown to make Bill jump
 document.addEventListener("keydown", (e) => {
   jump();
 });
