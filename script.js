3ab16a79561b099377ede23324951bcb64018503
Victor Gila Barreto <gilabarreto@gmail.com>
2023-01-22 17:14:19 -0700

    Change handleStart, create reset, create handleRun

--- script.js
+++ script.js
@@ -1,6 +1,57 @@
-const bill = document.getElementById("bill");
-const chair = document.getElementById("chair");
+const bill = document.querySelector("[data-bill");
+const chair = document.querySelector("[data-chair]");
+const start = document.querySelector("[data-start]");
+const scoreElem = document.querySelector("[data-score]");
 
+let score = 0;
+let isRunning = false;
+let billFrame = 0;
+
+document.addEventListener("keydown", handleStart);
+
+function handleStart() {
+  if (isRunning) {
+    return;
+  }
+  isRunning = true;
+  start.classList.add("hide")
+  chair.classList.add("run");
+  handleRun();
+  handleScore();
+  handleCollision();
+}
+
+// Function to reset the game
+function reset() {
+  score = 0;
+  scoreElem.innerHTML = score;
+  bill.src = "imgs/bill-still.png"
+  start.classList.remove("hide")
+  chair.classList.remove("run");
+}
+
+// Function to update score
+const handleScore = () => {
+  scoreInterval = setInterval(() => {
+    score += 1;
+    scoreElem.innerHTML = score;
+  }, 100);
+}
+
+// Function to handle run 
+const handleRun = () => {
+  if (bill.classList == "jump") {
+    bill.src = "imgs/bill-still.png"
+  }
+  runInterval = setTimeout(() => {
+    bill.src = `imgs/bill-${billFrame}.png`
+    if (billFrame === 0) {
+      billFrame = 1;
+    } else {
+      billFrame = 0;
+    } handleRun()
+  }, 100);
+}
 
 // Function to make Bill jump
 function jump() {
@@ -15,20 +66,29 @@ function jump() {
 }
 
 // Function to check if Bill hit the chair
-let isDead = setInterval(() => {
+const handleCollision = () => {
+  collisionInterval = setInterval(() => {
 
-  let billRect = bill.getBoundingClientRect();
-  let chairRect = chair.getBoundingClientRect();
+    const billRect = bill.getBoundingClientRect();
+    const chairRect = chair.getBoundingClientRect();
 
-  if (chairRect.left < billRect.right &&
-    chairRect.top < billRect.bottom &&
-    chairRect.right > billRect.left &&
-    chairRect.bottom > billRect.top) {
-    alert("Game Over")
-  }
-
-}, 10)
-
+    if (chairRect.left < billRect.right &&
+      chairRect.top < billRect.bottom &&
+      chairRect.right > billRect.left &&
+      chairRect.bottom > billRect.top) {
+      bill.src = "imgs/bill-dead.png"
+      isRunning = false;
+      clearInterval(runInterval)
+      clearInterval(collisionInterval)
+      clearInterval(scoreInterval)
+      setTimeout(() => {
+        if (confirm("Game Over")) {
+          reset()
+        }
+      }, 15)
+    }
+  }, 10)
+}
 
 // Event listener for keydown to make Bill jump
 document.addEventListener("keydown", (e) => {
